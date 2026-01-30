8ab7337925f743cd207b398ef367315068223adc
sandu343 <shansika343@gmail.com>
2026-01-30 23:27:02 +0530

    Tests 3,4,5 added and tested
    updated positive-functional.spec.js file with 3,4,5 test cases

--- tests/positive-functional.spec.js
+++ tests/positive-functional.spec.js
@@ -44,3 +44,68 @@ test('Pos_Fun_0002 - Convert compound sentence structure', async ({ page }) => {
   await expect(sinhalaOutput).toContainText('මම හැමදාම පන්ති යනවා, හැබැයි අද විභාගයක් තියෙන නිසා යන්න වෙන්නෙ නෑ');
 });
 
+//TEST 3
+test('Pos_Fun_0003 - Convert complex sentence structure', async ({ page }) => {
+  
+  // 1: Navigate to Swift Translator
+  await page.goto('https://www.swifttranslator.com');
+
+  // 2: Locate Singlish input field
+  const singlishInput = page.getByRole('textbox', {
+    name: 'Input Your Singlish Text Here.'
+  });
+
+  // 3: Enter Singlish text
+  await singlishInput.fill('mama aevidhinna yanna hari aasa unath vaeda vaedi nisaa velaavak naee');
+
+  // 4: Locate Sinhala output area
+  const sinhalaOutput = page.locator('.w-full.h-80.p-3.rounded-lg.ring-1.ring-slate-300.whitespace-pre-wrap'); 
+  
+
+  // 5: Verify Sinhala output is generated correctly
+  await expect(sinhalaOutput).toContainText('මම ඇවිදින්න යන්න හරි ආස උනත් වැඩ වැඩි නිසා වෙලාවක් නෑ');
+});
+
+//TEST 4
+test('Pos_Fun_0004 - Interrogative(questions) conversion', async ({ page }) => {
+  
+  // 1: Navigate to Swift Translator
+  await page.goto('https://www.swifttranslator.com');
+
+  // 2: Locate Singlish input field
+  const singlishInput = page.getByRole('textbox', {
+    name: 'Input Your Singlish Text Here.'
+  });
+
+  // 3: Enter Singlish text
+  await singlishInput.fill('oyaa mee gaena ammaava dhaenuvath karaadha?');
+
+  // 4: Locate Sinhala output area
+  const sinhalaOutput = page.locator('.w-full.h-80.p-3.rounded-lg.ring-1.ring-slate-300.whitespace-pre-wrap'); 
+  
+
+  // 5: Verify Sinhala output is generated correctly
+  await expect(sinhalaOutput).toContainText('ඔයා මේ ගැන අම්මාව දැනුවත් කරාද?');
+});
+
+//TEST 5
+test('Pos_Fun_0005 - Imparative(commands) conversion', async ({ page }) => {
+  
+  // 1: Navigate to Swift Translator
+  await page.goto('https://www.swifttranslator.com');
+
+  // 2: Locate Singlish input field
+  const singlishInput = page.getByRole('textbox', {
+    name: 'Input Your Singlish Text Here.'
+  });
+
+  // 3: Enter Singlish text
+  await singlishInput.fill('kiyavana eka navaththaganna');
+
+  // 4: Locate Sinhala output area
+  const sinhalaOutput = page.locator('.w-full.h-80.p-3.rounded-lg.ring-1.ring-slate-300.whitespace-pre-wrap'); 
+  
+
+  // 5: Verify Sinhala output is generated correctly
+  await expect(sinhalaOutput).toContainText('කියවන එක නවත්තගන්න');
+});
